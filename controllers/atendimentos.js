ca309773603ecb8b6d2ba2bb23d1b565b6ccf62e
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2022-02-11 18:13:25 -0300

    ADD POST ROUTE IN "ATENDIMENTOS.JS" ADD GET ROUTE IN "INDEX.JS"

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,5 +4,9 @@
 
 module.exports = app =>{
 
-app.get('/atendimentos', (req, res) => res.send('Rota de get para atendimentos.') ) // // TESTE NODEMON, DEPENDENCIA QUE FAZ COM QUE ATUALIZAÇÕES SEJAM ATUALIZADAS A PARTIR DO CTRL + S
+app.get('/atendimentos', (req, res) => res.send('Rota de get  atendimentos.') ) // // TESTE NODEMON, DEPENDENCIA QUE FAZ COM QUE ATUALIZAÇÕES SEJAM ATUALIZADAS A PARTIR DO CTRL + S
+
+
+app.post('/atendimentos', (req,res) => res.send('Rota de post atendimentos'))
+
 }
---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+// MÓDULO REFERENTE A PAGINA DE INICIAL
+
+// EXPORTAR MODULO
+
+module.exports = app =>{
+
+    app.get('/', (req, res) => res.send('Rota Padrão') ) // // TESTE NODEMON, DEPENDENCIA QUE FAZ COM QUE ATUALIZAÇÕES SEJAM ATUALIZADAS A PARTIR DO CTRL + S
+
+}
