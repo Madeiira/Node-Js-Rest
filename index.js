5f50a8bab388f8a689d5ee4c4d507334047ca178
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2022-02-14 16:04:26 -0300

    ajustando versionamento do mysql e melhorando regra de funcionamento

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const customExmpress = require('./config/customExpress')
 
 const conexao = require('./infrastructure/connection')
 
+conexao.connect(erro=>{
 
-const app = customExmpress();
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('conectando com sucesso')
 
-app.listen(3000, () => console.log('OK'))
+        const app = customExmpress();
+
+        app.listen(3000, () => console.log('OK'))
+    }
+
+}) 
+
