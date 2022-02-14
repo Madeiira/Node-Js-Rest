3e0f1f70386573cf6352b083dcee2b1582220412
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2022-02-14 15:45:14 -0300

    criando require connection

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const customExmpress = require('./config/customExpress')
 
+const conexao = require('./infrastructure/connection')
+
+
 const app = customExmpress();
 
 app.listen(3000, () => console.log('OK'))
