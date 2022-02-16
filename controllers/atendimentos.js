1653763137587ec19209903c51062a2e92ec2bb8
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2022-02-16 13:31:48 -0300

    ADICIONANDO COMENTÁRIOS

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -11,20 +11,19 @@ module.exports = app => {
     //app.get('/atendimentos', (req, res) => res.send('Rota de get  atendimentos.')) // // TESTE NODEMON, DEPENDENCIA QUE FAZ COM QUE ATUALIZAÇÕES SEJAM ATUALIZADAS A PARTIR DO CTRL + S
 
 
-    // ROTA GET DE RESGATE DE DADOS
-    app.get('/atendimentos', (req, res) => {
+    app.get('/atendimentos', (req, res) => { // ROTA GET DE RESGATE DE DADOS
         Atendimento.lista(res);
     });
 
-    //ROTA GET DE RESGATE DE DADO UNICO
-    app.get('/atendimentos/:id', (req, res) => {
+    
+    app.get('/atendimentos/:id', (req, res) => {//ROTA GET DE RESGATE DE DADO UNICO
         
         const reqId = parseInt(req.params.id)
 
         Atendimento.buscaPorId(reqId, res);
     });
 
-    app.post('/atendimentos', (req, res) => {
+    app.post('/atendimentos', (req, res) => { // ROTA DE CRIAR REGISTRO
 
 
         const corpo = req.body
